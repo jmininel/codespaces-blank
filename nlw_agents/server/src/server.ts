06269d7ca4b864c11a79795795cf7487fc1bd8f4
Juliana Mininel <57683000+jmininel@users.noreply.github.com>
2025-07-09 23:10:08 +0000

    feat : "created rooms and seed db"

--- nlw_agents/server/src/server.ts
+++ nlw_agents/server/src/server.ts
@@ -2,7 +2,8 @@ import { fastify } from 'fastify'
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod' 
 import { fastifyCors } from '@fastify/cors'
 import { env } from './env.ts'
-import { sql } from './db/connection.ts'
+
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -16,6 +17,8 @@ app.setValidatorCompiler(validatorCompiler)
 app.get('/health', () => {
     return 'ok'
 })
+
+app.register(getRoomsRoute)
 
 app.listen({ port: env.PORT}).then(() => {
    console.log('runinnf')
