65aa4c1636b972a8f0f8628778277e2a23ff20f0
Juliana Mininel <57683000+jmininel@users.noreply.github.com>
2025-07-10 22:54:03 +0000

    feat: "create questions and questions route"

--- nlw_agents/server/src/server.ts
+++ nlw_agents/server/src/server.ts
@@ -4,6 +4,8 @@ import { fastifyCors } from '@fastify/cors'
 import { env } from './env.ts'
 
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -28,6 +30,8 @@ app.get('/health', () => {
 })
 
 app.register(getRoomsRoute)
+app.register(createRoomRoute)
+app.register(getRoomQuestions)
 
 app.listen({
   port: env.PORT,
