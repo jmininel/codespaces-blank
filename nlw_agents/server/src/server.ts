25e7ad1065ef51b4b876cb2309396faab647018f
Juliana Mininel <57683000+jmininel@users.noreply.github.com>
2025-07-14 18:52:13 +0000

    feat: 'create chunks, upload audio with gemini'

--- nlw_agents/server/src/server.ts
+++ nlw_agents/server/src/server.ts
@@ -1,12 +1,14 @@
 import { fastify } from 'fastify'
-import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod' 
 import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod' 
 import { env } from './env.ts'
 
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
 import { createRoomRoute } from './http/routes/create-room.ts'
 import { getRoomQuestions } from './http/routes/get-room-questions.ts'
 import { createQuestionRoute } from './http/routes/create-question.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -23,6 +25,8 @@ app.register(fastifyCors, {
   maxAge: 86400
 })
 
+app.register(fastifyMultipart)
+
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
@@ -34,6 +38,7 @@ app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomQuestions)
 app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
 
 app.listen({
   port: env.PORT,
